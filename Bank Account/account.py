46f8d6d47a79978dceb633c0ecc5a8b4dc44b6fc
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-02-17 23:31:12 +0400

    Update account.py

--- Bank Account/account.py
+++ Bank Account/account.py
@@ -9,15 +9,40 @@ class Account:
         return self.account
     
     def deposite(self, user, depos):
-        if depos > 0:
-            self.account[user] += depos
+        if user not in self.account:
+            print("Username has not been found")
+        elif depos <= 0:
+            print('Only positive input')
         else:
-            return 'Only positive input'
-
+            self.account[user] += depos
+            
     def withdrow(self, user, withd):
         if withd > self.account[user]:
-            return 'There is no enough money'
+            print('There is no enough money')
         elif withd <= 0:
-            return 'Ask about positive amount'
+            print('Ask about positive amount')
+        elif user not in self.account:
+            print("Username has not been found")
         else:
             self.account[user] -= withd
+    
+    def registration(self):
+        user = input("Enter your name: ")
+        amount = int(input("Enter the amount: "))
+        a.user_(user, amount)
+    
+    def client(self):
+        us = input("For deposite type D, and for withdrow type W...: ")
+        if us == "D":
+            user = input("Enter your name: ")
+            amount = int(input("Enter deposite amount: "))
+            a.deposite(user, amount)
+        else:
+            user = input("Enter your name: ")
+            amount = int(input("Enter withdrow amount: "))
+            a.withdrow(user, amount)
+            
+# a = Account()
+# a.registration()
+# a.client()
+# print(a.getusers())
